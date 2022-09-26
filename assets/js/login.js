373dc9cca8f8ee2945d2bcf692a0b9698c2db6d9
zzq <1250450113@qq.com>
2022-09-26 21:14:35 +0800

    新建了本地仓库

---
+++ assets/js/login.js
@@ -0,0 +1,22 @@
+// 入口函数
+$(function() {
+// 点击事件
+$('#goReg').on('click',function() {
+   $('.reg-wrap').show()
+   $('.login-wrap').hide()
+})
+$('#gologin').on('click',function() {
+    $('.reg-wrap').hide()
+    $('.login-wrap').show()
+ })
+
+
+
+
+
+
+
+
+
+
+})
